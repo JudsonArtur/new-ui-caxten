09dcb4292a02d83d4e04b9d8e03415f4c0b7cad4
Judson Artur <judsoncapepa22@gmail.com>
2023-03-01 12:05:30 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+/* Informações do usuários no topo [ Abre e feicha o menu ]  */
+$('.user-info').on('click',function(e){
+     e.stopPropagation();
+     $(this).find('.user-menu').toggleClass('open')
+});
+
+$('body').on('click',function(e){
+     $('.user-menu').removeClass('open')
+});
+
+/* Tipos de pagamento */
+
+ /* MESES E ANO */
+
+$('.month-year-js label').on('click',function(){
+     $('.info-year').find('input').prop('checked',false);
+     $('.info-month').find('input').prop('checked',false);
+     $(this).find('input').prop('checked',true);
+     $('.month-year-js label').removeClass('active');
+     $(this).addClass('active');;
+
+     if( $('#info-year').is(":checked")){
+         $('.info-month-js').removeClass('open');
+         $('.info-year-js').addClass('open'); 
+         return  
+     }
+     $('.info-year-js').removeClass('open');
+     $('.info-month-js').addClass('open');  
+ }); 
+
+/* TRANSFERENCIA  E MULTICAIXA EXPRESS */
+
+$('.payment-types.p-way label').on('click',function(){
+     $('.pt--gateway').find('input').prop('checked',false);
+     $('.pt--transfer').find('input').prop('checked',false);
+     $(this).find('input').prop('checked',true);
+
+     if( $('#transfer').is(":checked")){
+         $('.gateway').removeClass('open');
+         $('.transfer').addClass('open'); 
+         return  
+     }
+     $('.transfer').removeClass('open');
+     $('.gateway').addClass('open');  
+ });
+
+ $('.plan-info--header').on('click', function(){
+          $(this).siblings('.plan-info--body').slideToggle();
+          let arr = $(this).find('.arrow').attr('name');
+          if(arr === 'chevron-down-outline'){
+               $(this).find('.arrow').attr('name','chevron-up-outline' );
+               return
+          }
+          $(this).find('.arrow').attr('name','chevron-down-outline' ); 
+          
+       
+ });
