71ab6d98270c42ecad23c98da468f4f07f4d7529
Judson Artur <judsoncapepa22@gmail.com>
2023-03-10 15:09:12 +0100

    paginas concluidas

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ $('.month-year-js label').on('click',function(){
      $('.info-month').find('input').prop('checked',false);
      $(this).find('input').prop('checked',true);
      $('.month-year-js label').removeClass('active');
-     $(this).addClass('active');;
+     $(this).addClass('active');
      $('#subtotal-price').html(`${$(this).data('price')} KZ`);
      $('#total-price').html(`${$(this).data('total')} KZ`);
      if( $('#info-year').is(":checked")){
@@ -35,7 +35,9 @@ $('.payment-types.p-way label').on('click',function(){
      $('.pt--gateway').find('input').prop('checked',false);
      $('.pt--transfer').find('input').prop('checked',false);
      $(this).find('input').prop('checked',true);
-
+     $('.transfer').removeClass('no-click'); 
+     $('.payment-types.p-way label').removeClass('active');
+     $(this).addClass('active');
      if( $('#transfer').is(":checked")){
          $('.gateway').removeClass('open');
          $('.transfer').addClass('open'); 
@@ -56,3 +58,15 @@ $('.payment-types.p-way label').on('click',function(){
           $(this).find('.arrow').attr('name','chevron-down-outline' ); 
  });
 
+ $('.faq-item').on('click', function () {
+     $(this).find('.faq-text').slideToggle();
+     $(this).toggleClass('active');
+     let arr = $(this).find('.faq-icon').attr('name');
+     console.log(arr);
+          if(arr === 'add-outline'){
+               $(this).find('.faq-icon').attr('name','remove-outline' );
+               return
+          }
+          $(this).find('.faq-icon').attr('name','add-outline' );
+
+ });
